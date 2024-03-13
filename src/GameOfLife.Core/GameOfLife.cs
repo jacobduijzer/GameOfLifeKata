29b9c564c59fa39c0a52e8d485c7fe493fc9785c
Jacob Duijzer <jacob@duijzer.com>
2024-03-13 16:05:25 +0100

    WIP: cycle 5

--- src/GameOfLife.Core/GameOfLife.cs
+++ src/GameOfLife.Core/GameOfLife.cs
@@ -6,53 +6,35 @@
         {
             var nextGeneration = string.Empty;
 
-            // *........
             for (var i = 0; i < input.Length; i++)
             {
+                if (input[i] == '.')
+                {
+                    nextGeneration += '.';
+                    continue;
+                }
+                
                 var nextNeighbourIndex = i + 1;
                 var previousNeighbourIndex = i - 1;
-                var numberOfLivingNeighbours = 0;
 
                 if (i == 0) // We don't have a previous neighbour
                 {
-                    if (input[nextNeighbourIndex] == '*')
-                        numberOfLivingNeighbours++;
+                    nextGeneration = ".";
                 }
                 else if (i == input.Length - 1) // We don't have a next neighbour
                 {
-                    if (input[previousNeighbourIndex] == '*')
-                        numberOfLivingNeighbours++;
-                }
+                    nextGeneration += ".";
+                } 
                 else
                 {
-                    if (input[nextNeighbourIndex] == '*')
-                        numberOfLivingNeighbours++;
-
-                    if (input[previousNeighbourIndex] == '*')
-                        numberOfLivingNeighbours++;
+                    if (input[previousNeighbourIndex] == '*' && input[nextNeighbourIndex] == '*')
+                        nextGeneration += "*";
+                    else
+                        nextGeneration += ".";
                 }
-
-                var status = CalculateIfCellDies(input[i] == '*', numberOfLivingNeighbours);
-                if (status)
-                    nextGeneration += ".";
-                else
-                    nextGeneration += "*";
             }
 
             return nextGeneration;
         }
-
-        private bool CalculateIfCellDies(
-            bool alive,
-            int numberOfLivingNeighbours)
-        {
-            if (alive && numberOfLivingNeighbours < 2)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
     }
 }
